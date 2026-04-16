90b675f85ac6f3441934df44c07481cae1e7115a
chatanyaO3 <chaitanyaagarwal139@gmail.com>
2026-04-16 09:55:52 +0000

    added input feature

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request
 import os
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return "🚀 Hello from Chaitanya's Web App!"
+    name = ""
+
+    if request.method == "POST":
+        user = request.form.get("username")
+        name = f"Hello {user} 👋"
+
+    return render_template("index.html", name=name)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
